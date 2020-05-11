62463b65ade41015f6e238d0f1e52a8ca1bc0ef7
dacastanogo <dcastanogomez@gmail.com>
2020-05-11 11:19:21 -0500

    Added minimum operations task

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+
+#!/usr/bin/python3
+
+
+"""
+Calculates the minimum operations necessary to reach a n quantity of characters
+starting with a single character and being only able to perform two actions
+copy all and paste
+"""
+
+
+def minOperations(n):
+    answer = 0
+    x = 2
+    while n > 1:
+        while n % x == 0:
+            answer += x
+            n /= x
+        x += 1
+    return answer
